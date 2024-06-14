a97428524a8dd1bcdb22ad087a9083700e6d27d4
JorgeGA17 <jorgegarciaalejo@gmail.com>
2024-06-14 16:16:41 -0500

    Direccionamiento con front

--- MacheNotas-api/src/main/java/com/MacheNotas/MacheNotas_api/controller/DocenteController.java
+++ MacheNotas-api/src/main/java/com/MacheNotas/MacheNotas_api/controller/DocenteController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/Docentes")
 @AllArgsConstructor
+@CrossOrigin(originPatterns = "http://localhost:4200/")
 
 public class DocenteController {
     private final DocenteService docenteService;
